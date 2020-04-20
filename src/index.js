2e4c9e10a990a4b19aa3b4d0e330b4df41711484
Marco <marcoasjunior90@gmail.com>
2020-04-20 15:40:49 -0300

    corsfix3

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const router = require('./router')
 
 // Express configs
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
+})
 app.use(express.json())
 
 // Routes 

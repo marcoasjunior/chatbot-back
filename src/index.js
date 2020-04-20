6e913c7fead5f752161eb3361f9a389eaf075948
Marco <marcoasjunior90@gmail.com>
2020-04-20 15:02:08 -0300

    corsfix

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 
 // Express configs
 
-app.use(cors())
 app.use(express.json())
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(cors())
 
 // Routes 
 

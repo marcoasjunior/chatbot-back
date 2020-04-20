75628dccb54c887f950fc4d969690e6b741c39e3
Marco <marcoasjunior90@gmail.com>
2020-04-20 14:54:49 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3333
 const router = require('./router')
+const cors = require('cors')
 
 // Express configs
 
+app.use(cors())
 app.use(express.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
